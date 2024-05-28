ccf867b5e39a8657f7e50961a93438e2e52e1c25
Antoine <91411073+T0ine34@users.noreply.github.com>
2024-05-28 12:48:47 +0200

    save update

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -81,11 +81,84 @@ enum SENSITIVE_LEVELS{
     SHOW
 }
 
+enum TargetType{
+    FILE,
+    TERMINAL
+}
+
+class Target{
+    private static instances : Record<string, Target> = {};
+    private _target : Function = (x : string) => {};
+    private _name : string = "";
+    private _properties : Record<string, any> = {};
+    private _type : TargetType = TargetType.FILE;
+
+
+    constructor(targetFunc: Function, name: string|null = null){
+        if(name == null){
+            name = targetFunc.name;
+        }
+        if(Target.instances[name]){
+            return Target.instances[name];
+        }
+
+        this._target = targetFunc;
+        this._name = name;
+        if(targetFunc === console.log){
+            this._type = TargetType.TERMINAL;
+        }
+        else{
+            this._type = TargetType.FILE;
+        }
+
+        Target.instances[name] = this;
+    }
+
+    static fromFile(path: string){
+        fs.writeFileSync(path, "");
+        return new Target((message : string) => {
+            fs.appendFileSync(path, message + '\n');
+        }, path);
+    }
+
+    call(message: string){
+        this._target(message);
+    }
+
+    toString(){
+        return this._name;
+    }
+
+    getProperty(key: string){
+        return this._properties[key];
+    }
+
+    setProperty(key: string, value: any){
+        this._properties[key] = value;
+    }
+
+    get type(){
+        return this._type;
+    }
+
+    static get(targetName: string){
+        if(Target.instances[targetName]){
+            return Target.instances[targetName];
+        }
+        throw new Error("Target '" + targetName + "' not found");
+    }
+
+    static exist(targetName: string){
+        return Target.instances[targetName] != null;
+    }
+}
+
 class Logger{
     static _instance = new Logger();
 
-    private _level : LEVELS.LEVELS = LEVELS.LEVELS.INFO;
-    private _target : Function = console.log;
+    // private _level : LEVELS.LEVELS = LEVELS.LEVELS.INFO;
+    // private _target : Function = console.log;
+    private _targets : Target[] = [];
     private _sensitive_level : SENSITIVE_LEVELS = SENSITIVE_LEVELS.HIDE;
     private _sensitive_data : string[] = [];
     private _module_map : Record<string, string> = {}; // filepath -> module name
@@ -96,34 +169,35 @@ class Logger{
         }
         Logger._instance = this;
 
+        Logger._instance._targets.push(new Target(console.log, "terminal"));
+        Target.get("terminal").setProperty('level', LEVELS.LEVELS.INFO);
+
         return this;
     }
 
 // ----------------- CONFIGURATION METHODS -----------------
 
-    static setLevel(level: LEVELS.LEVELS){
-        Logger._instance._level = level;
+    static setLevel(targetName: string, level: LEVELS.LEVELS){
+        let target = Target.get(targetName);
+        target.setProperty('level', level);
     }
 
-    static setTarget(target: any){
-        // target can be either a function or a file path; if it's a file path, it will be created if it doesn't exist
-        if(typeof target === 'string'){
-            // create the file if it doesn't exist, clear it if it does
-            let path = target.toString();
-            if(fs.existsSync(path)){
-                fs.unlinkSync(path);
+    static addTarget(targetSource : string|Function){
+        let target : Target;
+        if(typeof targetSource === 'string'){
+            if(Target.exist(targetSource)){
+                throw new Error("Target '" + targetSource + "' already exists");
             }
-            fs.writeFileSync(path, '');
-            Logger._instance._target = (message : string) => {
-                fs.appendFileSync(path, message + '\n');
-            }
+            target = Target.fromFile(targetSource);
         }
-        else if(typeof target === 'function'){
-            Logger._instance._target = target;
+        else if(typeof targetSource === 'function'){
+            target = new Target(targetSource);
         }
         else{
-            throw new Error("Invalid target type");
+            throw new Error("Invalid target source");
         }
+        Logger._instance._targets.push(target);
+        target.setProperty('level', LEVELS.LEVELS.INFO);
     }
 
     static setSensitiveLevel(level: SENSITIVE_LEVELS){
@@ -143,59 +217,68 @@ class Logger{
 
 // ------------------- INTERNAL METHODS --------------------
 
-    static #log(level : LEVELS.LEVELS, message : string, filename : string){
-        if(Logger._instance._level <= level){
-        Logger._instance._target(Logger.#format(level, message, filename));
+    private static log(level : LEVELS.LEVELS, message : string, filename : string){
+        for(let target of Logger._instance._targets){
+            console.log(target.toString(), target.getProperty('level'), level);
+            if(target.getProperty('level') <= level){
+                let result = "";
+                let moduleName = Logger._instance._module_map[filename];
+                let indent = 33 + (moduleName ? 15 : 0);
+                message = replaceNewLine(message, indent);
+                if(target.type === TargetType.TERMINAL){
+                    result = `[${COLORS.BLUE.toString()}${getTime()}${COLORS.RESET.toString()}] [${LEVELS.getColor(level)}${LEVELS.toString(level)}${COLORS.RESET.toString()}] `;
+                    if(moduleName){
+                        result += `[ ${COLORS.BLUE.toString()}${centerString(moduleName, 10)}${COLORS.RESET.toString()} ] `;
+                    }
+                }
+                else{
+                    //same as terminal but without colors
+                    result = `[${getTime()}] [${LEVELS.toString(level)}] `;
+                    if(moduleName){
+                        result += `[ ${centerString(moduleName, 10)} ] `;
+                    }
+                }
+                result += message;
+                target.call(result);
+            }
         }
-    }
-
-    static colorString(color : COLORS, message : string){
-        if(Logger._instance._target === console.log){
-            return color.toString() + message + COLORS.RESET.toString();
-        }
-        return message; // no color support for file output
-    }
-
-    static #format(level : LEVELS.LEVELS, message : string, filename : string){
-        let moduleName = Logger._instance._module_map[filename];
-        let indent = 33 + (moduleName ? 15 : 0);
-        message = replaceNewLine(message, indent);
-        let result = `[${Logger.colorString(COLORS.BLUE, getTime())}] [${Logger.colorString(LEVELS.getColor(level), LEVELS.toString(level))}] `;
-        if(moduleName){
-            result += `[ ${Logger.colorString(COLORS.BLUE, centerString(moduleName, 10))} ] `;
-        }
-        result += message;
-        return result;
     }
 
 // ------------------- LOGGING METHODS ---------------------
 
     static message(message : string, color = COLORS.NONE){
-        Logger._instance._target(Logger.colorString(color, message));
+        for(let target of Logger._instance._targets){
+            if(target.type === TargetType.TERMINAL){
+                target.call(color.toString() + message + COLORS.RESET.toString());
+            }
+            else{
+                target.call(message);
+            }
+        }
     }
 
     static deepDebug(message : string, filename = GetCallerInfo()){
-        Logger.#log(LEVELS.LEVELS.DEEP_DEBUG, message, filename);
+        Logger.log(LEVELS.LEVELS.DEEP_DEBUG, message, filename);
     }
 
     static debug(message : string, filename = GetCallerInfo()){
-        Logger.#log(LEVELS.LEVELS.DEBUG, message, filename);
+        Logger.log(LEVELS.LEVELS.DEBUG, message, filename);
     }
     
     static info(message : string, filename = GetCallerInfo()){
-        Logger.#log(LEVELS.LEVELS.INFO, message, filename);
+        Logger.log(LEVELS.LEVELS.INFO, message, filename);
     }
 
     static warning(message : string, filename = GetCallerInfo()){
-        Logger.#log(LEVELS.LEVELS.WARNING, message, filename);
+        Logger.log(LEVELS.LEVELS.WARNING, message, filename);
     }
 
     static error(message : string, filename = GetCallerInfo()){
-        Logger.#log(LEVELS.LEVELS.ERROR, message, filename);
+        Logger.log(LEVELS.LEVELS.ERROR, message, filename);
     }
 
     static critical(message : string, filename = GetCallerInfo()){
-        Logger.#log(LEVELS.LEVELS.CRITICAL, message, filename);
+        Logger.log(LEVELS.LEVELS.CRITICAL, message, filename);
     }
 }
 

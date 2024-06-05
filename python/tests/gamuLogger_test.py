3630036280bda23152d0356dbf8086a9fd280142
Antoine Buirey <91411073+T0ine34@users.noreply.github.com>
2024-06-05 13:01:30 +0000

    fixing warning

--- python/tests/gamuLogger_test.py
+++ python/tests/gamuLogger_test.py
@@ -6,9 +6,7 @@ import re
 import tempfile
 from time import sleep
 
-from gamuLogger import Logger, deepDebug, debug, info, warning, error, critical, message, deepDebugFunc, debugFunc, chrono, LEVELS, TERMINAL_TARGETS, SENSITIVE_LEVELS
-
-# from ..src.gamuLogger import Logger, deepDebug, debug, info, warning, error, critical, message, deepDebugFunc, debugFunc, chrono, LEVELS, TERMINAL_TARGETS
+from gamuLogger import Logger, deepDebug, debug, info, warning, error, critical, message, deepDebugFunc, debugFunc, chrono, LEVELS, TERMINAL_TARGETS, SENSITIVE_LEVELS #type: ignore
 
 class Test_Logger:
     def test_deepDebug(self, capsys):

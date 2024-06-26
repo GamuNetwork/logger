223f0ab960683af8de7ea607ddd982a5a6db117f
Antoine Buirey <91411073+T0ine34@users.noreply.github.com>
2024-06-26 19:09:07 +0200

    trying to fix CI

--- javascript/pack.py
+++ javascript/pack.py
@@ -1,20 +1,13 @@
-from builderTool import BaseBuilder, NULL_TARGET, Logger
-import shutil, os
-from tempfile import mkstemp
+from builderTool import BaseBuilder, Logger
 
 class Builder(BaseBuilder):
     def Setup(self):
-        Logger.debug("Copying LICENSE, package.json and package-lock.json")
-        shutil.copyfile("../LICENSE", self.tempDir+"/LICENSE")
-        self.CopyAndReplaceByPackageVersion("package.json", self.tempDir+"/package.json")
-        self.CopyAndReplaceByPackageVersion("package-lock.json", self.tempDir+"/package-lock.json")
-        
-        Logger.debug("Copying config files")
-        shutil.copytree("config", self.tempDir+"/config")
-        
-        # copy sources
-        Logger.debug("Copying sources")
-        shutil.copytree("src", self.tempDir+"/src")        
+        self.addFile("../LICENSE", "/LICENSE")
+        self.addAndReplaceByPackageVersion("package.json", self.tempDir+"/package.json")
+        self.addAndReplaceByPackageVersion("package-lock.json", self.tempDir+"/package-lock.json")
+        self.addDirectory("config")
+        self.addDirectory("src")       
+        self.addFile("tsconfig.json") 
         
         #install node modules
         Logger.debug("Installing node modules")
@@ -22,19 +15,12 @@ class Builder(BaseBuilder):
         
         
     def Build(self):
-        
-        Logger.debug("copying tsconfig.json")
-        shutil.copyfile("tsconfig.json", self.tempDir+"/tsconfig.json")
-        
-        Logger.debug("Building project")
         return self.runCommand("npm run build")
         
         
         
     def Tests(self):
-        Logger.debug("copying tests")
-        shutil.copytree("tests", self.tempDir+"/tests")
-        Logger.debug("Running tests")
+        self.addDirectory("tests")
         return self.runCommand(f"npm test")
     
     

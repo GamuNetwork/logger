133b7e58657a7883ac45874eebbf26a128fca365
Antoine <91411073+T0ine34@users.noreply.github.com>
2024-10-29 19:51:24 +0100

    updating to feanor V1.2.4

--- python/pack.py
+++ python/pack.py
@@ -9,12 +9,11 @@ class Builder(BaseBuilder):
         self.addAndReplaceByPackageVersion('pyproject.toml')
         self.addFile('../LICENSE', 'LICENSE')
         
-        self.venv().install('build==1.2.1')
-        self.venv().install('pytest==8.2.2')
-        
     def Build(self):
-        self.venv().runModule(f"build {self.tempDir} --outdir {self.distDir}")
-    
+        self.venv().install('build')
+        self.venv().runModule('build', '--outdir', f"{self.distDir}")
+        
     def Tests(self):
         self.addDirectory('tests')
-        self.venv().runModule(f"pytest {self.tempDir}/tests")
+        self.venv().install('pytest')
+        self.venv().runModule('pytest', f"{self.tempDir}/tests")
